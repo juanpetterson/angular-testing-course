96837d0b22bfb2b680dcda2f9da194526c330517
juanpetterson <juan.petterson@gmail.com>
2020-04-01 19:40:19 -0300

    Isolating tests with beforeEach

--- src/app/courses/services/calculator.service.spec.ts
+++ src/app/courses/services/calculator.service.spec.ts
@@ -1,28 +1,28 @@
 import { CalculatorService } from './calculator.service';
-import { LoggerService } from './logger.service';
 
 describe('CalculatorService', () => {
+  let calculator: CalculatorService;
+  let loggerSpy: any;
+
+  beforeEach(() => {
+    loggerSpy = jasmine.createSpyObj('LoggerService', ['log']);
+
+    calculator = new CalculatorService(loggerSpy);
+  });
+
   it('should add two numbers', () => {
-    const logger = jasmine.createSpyObj('LoggerService', ['log']);
-
-    const calculator = new CalculatorService(logger);
-
     const result = calculator.add(2, 2);
 
     expect(result).toBe(4);
 
-    expect(logger.log).toHaveBeenCalledTimes(1);
+    expect(loggerSpy.log).toHaveBeenCalledTimes(1);
   });
 
   it('should subtract two numbers', () => {
-    const logger = jasmine.createSpyObj('LoggerService', ['log']);
-
-    const calculator = new CalculatorService(logger);
-
     const result = calculator.subtract(2, 2);
 
     expect(result).toBe(0, 'unexpected subtration result');
 
-    expect(logger.log).toHaveBeenCalledTimes(1);
+    expect(loggerSpy.log).toHaveBeenCalledTimes(1);
   });
 });
